656080afd0646fe20ede5ffc9bee962e5326a363
MateusMM23 <mateus.monteiro23@hotmail.com>
2025-10-22 08:56:21 -0300

    Desafio 2 finalizado

---
+++ DESAFIO-2.js
@@ -0,0 +1,19 @@
+let 
+
+
+
+if(xp < 10){
+  nivel == "ferro"
+}else if(xp >= 11 && xp <= 20){
+  nivel = "bronze"
+}else if(xp >= 21 && xp <= 50){
+  nivel = "prata"
+}else if(xp >= 51 && xp <= 80){
+  nivel = "ouro"
+}else if(xp >= 81 && xp <= 90){
+  nivel = "diamante"
+}else if(xp >= 91 && xp <= 100){
+  nivel = "lendário"
+}else if(xp >= 101){
+  nivel = "imortal" 
+}
